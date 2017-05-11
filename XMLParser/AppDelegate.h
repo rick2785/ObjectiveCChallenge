5c50b968654afb0ce72249d721302e6597059d34
Rickey Hrabowskie <rickeyhrabowskie@Rickeys-MacBook-Pro.local>
2017-05-11 13:37:16 -0500

    Initial Commit

---
+++ XMLParser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMLParser
+//
+//  Created by Rickey Hrabowskie on 5/11/17.
+//  Copyright © 2017 Rickey Hrabowskie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMLParser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMLParser
+//
+//  Created by Rickey Hrabowskie on 5/11/17.
+//  Copyright © 2017 Rickey Hrabowskie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
